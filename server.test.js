0cd89e88ecd36ee1625fcddc3758987a73fb3afc
Jonathan Heinz <jonathan.heinz@aol.com>
2019-01-31 14:59:13 -0800

    wrote tests for POST route

--- server.test.js
+++ server.test.js
@@ -13,11 +13,31 @@ describe('server', () => {
 
     });
 
-    it('should return an array of data', () => {
+    it('should return an array of data', async () => {
 
       const response = await request(server).get('/api/dogs');
 
       expect(Array.isArray(response.data)).toEqual(true);
+
+    });
+
+  });
+
+  describe('POST /api/dogs', () => {
+
+    it('should return status code of 201', async () => {
+
+      const response = await request(server).post('/api/dogs', { name: 'Poodle' });
+
+      expect(response.status).toEqual(201);
+
+    });
+
+    it('should return the id of the newly created dog', async () => {
+
+      const response = await request(server).post('/api/dogs', { name: 'German Shepherd'});
+
+      expect(response.data.id).not.toEqual(null);
 
     });
 
